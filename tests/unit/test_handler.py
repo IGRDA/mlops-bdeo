fd2b3995cf6b8d879143c994c9c65f73977a46d4
Iñaki <inaki.gorostiaga@smowltech.com>
2022-07-14 18:34:14 +0200

    asdaddjsjjzs

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -8,6 +8,7 @@ from function  import inference
 #@pytest.fixture()
 def test_lambda_handler():
     ret = inference.lambda_handler("","")
+    print()
     #data = json.loads()
     #assert ret["statusCode"] == 200
     #assert "message" in ret["body"]
