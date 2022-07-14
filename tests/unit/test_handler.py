28c57faf1a805b787fce1687b478e93d6575dc94
Iñaki <inaki.gorostiaga@smowltech.com>
2022-07-14 19:07:45 +0200

    1857

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -3,11 +3,11 @@ import os
 from function  import inference
 #import json
 
-@pytest.fixture()
+os.environ["Env"] = "dev"
+#@pytest.fixture()
 def test_lambda_handler():
     ret = inference.lambda_handler("","")
-    print(ret)
-#data = json.loads()
-#assert ret["statusCode"] == 200
-#assert "message" in ret["body"]
-#assert data["message"] == "hello world"
+    #data = json.loads()
+    #assert ret["statusCode"] == 200
+    #assert "message" in ret["body"]
+    #assert data["message"] == "hello world"
