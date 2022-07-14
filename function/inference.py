44b0ef47f762fc12b2ecf5739cc0e8d09d217159
Iñaki <inaki.gorostiaga@smowltech.com>
2022-07-14 19:40:43 +0200

    1940

--- function/inference.py
+++ function/inference.py
@@ -6,21 +6,21 @@ from boto3 import resource as boto3_resource
 if os.environ.get("Env") is None:
     os.environ["Env"] = "dev"
 
-dynamodb = boto3_resource('dynamodb',"eu-west-3")
+dynamodb = boto3_resource('dynamodb', "eu-west-3")
 table = dynamodb.Table("rekognitionObjectDet-{}".format(os.environ["Env"]))
 ts = torch.jit.load('function/doubleit_model.pt')
+
 
 def lambda_handler(event, context):
     print(event)
     # Example or inference. This model returns a 1-dim tensor multiplied by 2
     sample_tensor = torch.tensor([1, 2, 3, 4])
     result = ts(sample_tensor)
-    print(round(uuid.uuid4().int,10))
-    """
+    print(round(uuid.uuid4().int, 10))
+"""
     response = table.put_item(
         Item={"id": int,
               "result": result.tolist()}
-    )
-    """
+)"""
     
     print(result)  # <- tensor([2, 4, 6, 8])
